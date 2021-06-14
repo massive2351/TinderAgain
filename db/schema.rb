cb4bf116475f0d22802faca81732e0883de1557c
massive2351 <massive2351@gmail.com>
2021-06-14 07:58:07 +0000

    プロフィール画像のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_073650) do
+ActiveRecord::Schema.define(version: 2021_06_14_075726) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_06_11_073650) do
     t.string "name"
     t.integer "gender", default: 0
     t.text "self_introduction"
+    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
